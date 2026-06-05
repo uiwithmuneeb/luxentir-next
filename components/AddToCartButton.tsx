44c7593105b494106c65b92193ed84f50bf86f3e
uiwithmuneeb <uiwithmuneeb@gmail.com>
2026-06-06 04:45:47 +0500

    Update AddToCartButton.tsx

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -6,17 +6,19 @@ export default function AddToCartButton({
   id,
   size = "S",
   color = "Ivory",
+  image,
 }: {
   id: number;
   size?: string;
   color?: string;
+  image: string;
 }) {
   const { addToCart } = useCart();
 
   return (
     <button
       className="btn gold"
-      onClick={() => addToCart(id, size, color)}
+      onClick={() => addToCart(id, size, color, image)}
     >
       Add to Cart
     </button>
