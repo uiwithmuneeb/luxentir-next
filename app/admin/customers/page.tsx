5315fc2da63bd8d58439a4ae7bc6b1bc0700d726
uiwithmuneeb <uiwithmuneeb@gmail.com>
2026-06-13 04:08:00 +0500

    fix cache

--- app/admin/customers/page.tsx
+++ app/admin/customers/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { prisma } from "@/lib/prisma";
 import AdminCustomersClient from "@/components/admin/AdminCustomersClient";
 
--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { prisma } from "@/lib/prisma";
 import AdminOrdersClient from "@/components/admin/AdminOrdersClient";
 
